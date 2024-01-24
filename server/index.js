fd2724db073d0313db326995eba53c8d0dfcd69c
NULL0M <96276811+NULL0M@users.noreply.github.com>
2024-01-24 10:45:22 +0100

    I created the post and dalle routes the dalle is the api from open ai to genrate images and post to craet posts in the database

--- server/index.js
+++ server/index.js
@@ -5,11 +5,23 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
 
 
 const startServer = async () => {
