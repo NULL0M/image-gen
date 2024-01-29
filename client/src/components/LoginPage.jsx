1ac6265e09e3a6b48886d29d382d45d454db3698
Alexandra <aleksandra.adamchik@gmail.com>
2024-01-29 09:14:29 +0100

    added registering and login part

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,63 +1,87 @@
 // client/src/components/LoginPage.jsx
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { MyContext } from '../contexts/context';
-import { IoMdCloseCircle } from 'react-icons/io';
-import './LoginPage.scss';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { MyContext } from "../contexts/context";
+import { IoMdCloseCircle } from "react-icons/io";
+import "./LoginPage.scss";
 
 export default function LoginPage({ openRegisterModal }) {
   const { setUser, closeModal } = useContext(MyContext);
 
-  const loginUser = (e) => {
+  const loginUser = async (e) => {
     e.preventDefault();
+
     const loginInfo = {
-      email: e.target.email.value,
+      username: e.target.username.value,
       password: e.target.password.value,
     };
+    console.log("Login Info", loginInfo);
 
-    // Feche o modal de login
-    onClose();
+    // FETCHING USER LOGIN AND PASSWORD FROM SERVER, IF THEY DON´T EXIST, GIVING AN ERROR
+    try {
+      const response = await fetch("http://localhost:8090/api/v1/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginInfo),
+      });
+
+      if (response.ok) {
+        //login successful, display success alert
+        alert("Login successful!");
+        closeModal(); // Close the modal or redirect the user as needed
+      } else {
+        // Login failed, display error alert
+        alert("Login failed. Please check your credentials and try again.");
+      }
+    } catch (error) {
+      // Handle network errors or other issues
+      console.error("Error during login:", error);
+      alert("Error during login. Please try again.");
+    }
   };
 
   return (
-    <div className='loginpage'>
-      <IoMdCloseCircle className='close-button' onClick={closeModal} />
-      <Link className='loginpage-child' to='/' />
-      <div className='welcome-back-parent'>
-        <h2 className='welcome-back'>{`Welcome  back `}</h2>
-        <p className='please-enter-your-email'>
+    <div className="loginpage">
+      <IoMdCloseCircle className="close-button" onClick={closeModal} />
+      <Link className="loginpage-child" to="/" />
+      <div className="welcome-back-parent">
+        <h2 className="welcome-back">{`Welcome  back `}</h2>
+        <p className="please-enter-your-email">
           Please enter your Email and Password to log in
         </p>
       </div>
-      <form className='email-form' action='' onSubmit={loginUser}>
-        <label className='email2'>User Name</label>
-        <span className='email-container'>
+      <form className="email-form" action="" onSubmit={loginUser}>
+        <label className="email2">User Name</label>
+        <span className="email-container">
           <input
-            className='email3'
-            type='text'
-            id='username'
-            name='username'
-            placeholder='User Name'
+            className="email3"
+            type="text"
+            id="username"
+            name="username"
+            placeholder="User Name"
           />
         </span>
-        <label className='email2'>Password</label>
-        <span className='email-container'>
+        <label className="email2">Password</label>
+        <span className="email-container">
           <input
-            className='email3'
-            type='password'
-            id='password'
-            name='password'
-            placeholder='Password'
+            className="email3"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
           />
         </span>
+        <button type="submit" className="log-in-wrapper">
+          <div className="log-in">Log In</div>
+        </button>
       </form>
-      <button className='log-in-wrapper'>
-        <div className='log-in'>Log In</div>
-      </button>
-      <div className='dont-have-an-account-yet-parent'>
-        <h3 className='dont-have-an'>Don't have an account yet?</h3>
-        <button className='registerButton' onClick={openRegisterModal}>
-          <div className='loregisterButtonSubmit'>Create account</div>
+
+      <div className="dont-have-an-account-yet-parent">
+        <h3 className="dont-have-an">Don't have an account yet?</h3>
+        <button className="registerButton" onClick={openRegisterModal}>
+          <div className="loregisterButtonSubmit">Create account</div>
         </button>
       </div>
     </div>
---
+++ server/mongodb/models/Users.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        /* min: 3,
+        max: 20, */
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        /* max: 50, */
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        
+    }, 
+});
+
+export const UserModel = mongoose.model("users", UserSchema);
